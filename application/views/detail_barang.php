ffc623a48d410db92c1d677860a39c0b3cd5c53c
mwalidnurfadhil22 <173040079@mail.unpas.ac.id>
2019-12-14 10:07:48 +0700

    merubah url tomol kembali, merapikan tampilan

--- application/views/detail_barang.php
+++ application/views/detail_barang.php
@@ -1,46 +1,46 @@
 <div class="container-fluid">
 	<div class="card">
 		<div class="card">
-		  <div class="card-header">
-		    Detail Komik
-		  </div>
-		  <div class="card-body">
+		  	<div class="card-header">
+		    	Detail Komik
+		  	</div>
+		 	<div class="card-body">
 
-		  	<?php foreach ($barang as $brg): ?>
-		   		<div class="row">
-		   			<div class="col-md-4">
-		   				<img width="300px" height="400px" src="<?php echo base_url().'/assets/img/'.$brg->gambar ?>" class="card-imd-top">
-		   			</div>
-		   			<div class="col-md-8">
-		   				<table class="table">
-		   					<tr>
-		   						<td>Nama Komik</td><td><strong><?php echo $brg->judul?></strong></td>
-		   					</tr>
-		   					<tr>
-		   						<td>penulis</td><td><strong><?php echo $brg->penulis?></strong></td>
-		   					</tr>
-							<tr>
-		   						<td>Penerbit</td><td><strong><?php echo $brg->penerbit?></strong></td>
-		   					</tr>
-		   					<tr>
-		   						<td>Kategori</td><td><strong><?php echo $brg->kategori?></strong></td>
-		   					</tr>
-							<tr>
-		   						<td>Tahun Terbit</td><td><strong><?php echo $brg->thn_terbit?></strong></td>
-		   					</tr>
-		   					<tr>
-		   						<td>Stok</td><td><strong><?php echo $brg->stok?></strong></td>
-		   					</tr>
-		   					<tr>
-		   						<td>Harga</td><td><strong><div class="btn btn-sm btn-success">Rp. <?php echo number_format($brg->harga,0, ',','.') ?></div></strong></td>
-		   					</tr>
-		   				</table>
-		   				<?php echo anchor('home/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
-		   				<?php echo anchor('home/index/','<div class="btn btn-sm btn-danger">Kembali</div>') ?>
-		   			</div>
-		   		</div>
-		   	<?php endforeach; ?>
-		  </div>
-</div>
-
+				<?php foreach ($barang as $brg): ?>
+					<div class="row">
+						<div class="col-md-4">
+							<img width="300px" height="400px" src="<?php echo base_url().'/assets/img/'.$brg->gambar ?>" class="card-imd-top">
+						</div>
+						<div class="col-md-8">
+							<table class="table">
+								<tr>
+									<td>Judul</td><td><strong><?php echo $brg->judul?></strong></td>
+								</tr>
+								<tr>
+									<td>Penulis</td><td><strong><?php echo $brg->penulis?></strong></td>
+								</tr>
+								<tr>
+									<td>Penerbit</td><td><strong><?php echo $brg->penerbit?></strong></td>
+								</tr>
+								<tr>
+									<td>Kategori</td><td><strong><?php echo $brg->kategori?></strong></td>
+								</tr>
+								<tr>
+									<td>Tahun Terbit</td><td><strong><?php echo $brg->thn_terbit?></strong></td>
+								</tr>
+								<tr>
+									<td>Stok</td><td><strong><?php echo $brg->stok?></strong></td>
+								</tr>
+								<tr>
+									<td>Harga</td><td><strong><div class="btn btn-sm btn-success">Rp. <?php echo number_format($brg->harga,0, ',','.') ?></div></strong></td>
+								</tr>
+							</table>
+							<?php echo anchor('home/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Tambah ke Keranjang</div>') ?>
+							<?php echo anchor('home','<div class="btn btn-sm btn-danger">Kembali</div>') ?>
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
 </div>
